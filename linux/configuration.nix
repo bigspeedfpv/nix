f2b900a2585f154926955e07319adfd285e68e52
Andrew Brower <bigspeedfpv@gmail.com>
2024-03-12 01:19:58 -0400

    fix: disable pass auth

--- linux/configuration.nix
+++ linux/configuration.nix
@@ -12,13 +12,18 @@
     users.users.andy = {
         isNormalUser = true;
         extraGroups = ["wheel"];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKsuRGxm3xKVrfclQDfv1Q4OvUFCBwO+Gm97qm8LKVo" ];
     };
 
     environment.systemPackages = with pkgs; import ../config/global-packages.nix pkgs inputs ++ [
         wget
     ];
 
-    services.openssh.enable = true;
+    services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+        settings.KbdInteractiveAuthentication = false;
+    };
 
     system.stateVersion = "23.11";
 }
